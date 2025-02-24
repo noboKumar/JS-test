f4a8a746555290bf39f3ab5fb19c8f39ec92357b
noboKumar <nobokumar227@gmail.com>
2025-02-24 14:09:51 +0600

    add practice 1

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn").addEventListener('click',function(){
+    const titleText = document.getElementById("title");
+    titleText.innerText = "Wow! you just clicked Me."
+});
