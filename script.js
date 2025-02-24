76157eea2abb75d7475941e670d6aa21671de647
noboKumar <nobokumar227@gmail.com>
2025-02-24 14:18:03 +0600

    add practice2

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+// Events: practice-1
 document.getElementById("btn").addEventListener('click',function(){
     const titleText = document.getElementById("title");
     titleText.innerText = "Wow! you just clicked Me."
 });
+
+//practice-2
+function redColor() {
+    document.body.style.backgroundColor = "red";
+}
